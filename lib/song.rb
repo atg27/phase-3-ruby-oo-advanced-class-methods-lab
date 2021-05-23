ba9b5b3c1500088b153dc30b82bb368109908439
Andrew <atg27@uw.edu>
2021-05-22 19:12:53 -0700

    commit tree 2244126f065a933f0b449f8887e38476436b3b25

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,58 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+def self.new_by_name(name)
+  song = self.new
+  song.name = name
+  song
+end
+
+def self.create_by_name(name)
+  song = self.create
+  song.name = name
+  song
+end
+
+def self.find_by_name(name)
+  @@all.detect{|s| s.name == name}
+end
+
+def self.find_or_create_by_name(name)
+  self.find_by_name(name) || self.create_by_name(name)
+    
+end
+
+def self.alphabetical
+  @@all.sort_by{|song| song.name}
+end
+
+def self.new_from_filename(file)
+  names = file.split(" - ")
+  artist_name = names[0]
+  song_name = names[1].gsub(".mp3", '')
+
+  song = self.new
+  song.name = song_name
+  song.artist_name = artist_name
+  song
+end
+def self.create(file)
+  names = file.split(" - ")
+  artist_name = names[0]
+  song_name = names[1].gsub(".mp3", '')
+
+  song = self.create
+  song.name = song_name
+  song.artist_name = artist_name
+  song
+end
+# def self.destroy_all
+#   self.all.clear
+# end
 end
